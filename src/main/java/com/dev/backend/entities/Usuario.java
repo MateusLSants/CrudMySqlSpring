82e1f3e19a7cc6913d84cc0f89a44e998561045b
Mateus Leandro <100031728+MateusLSants@users.noreply.github.com>
2023-02-13 07:03:52 -0300

    implementado crud de usuario

---
+++ src/main/java/com/dev/backend/entities/Usuario.java
@@ -0,0 +1,40 @@
+package com.dev.backend.entities;
+
+import java.util.Date;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+    private String endereco;
+    private String cep;
+
+    @ManyToOne
+    private Cidade cidade;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAtualizacao;
+}
---
+++ src/main/java/com/dev/backend/services/UsuarioServices.java
@@ -0,0 +1,37 @@
+package com.dev.backend.services;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dev.backend.entities.Usuario;
+import com.dev.backend.repository.UsuarioRepository;
+
+@Service
+public class UsuarioServices {
+    
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario inserir(Usuario usuario) {
+        usuario.setDataCriacao(new Date());
+        Usuario usuarioNovo = usuarioRepository.saveAndFlush(usuario);
+        return usuarioNovo;
+    }
+
+    public Usuario alterar(Usuario usuario) {
+        usuario.setDataAtualizacao(new Date());
+        return usuarioRepository.saveAndFlush(usuario);
+    }
+
+    public void excluir(Long id) {
+        Usuario usuario = usuarioRepository.findById(id).get();
+        usuarioRepository.delete(usuario);
+    }
+}
